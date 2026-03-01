b05491582f6a51148c641bf273938ef9a411183c
Mr007 <s25barin1m01118@iub.edu.pk>
2026-03-02 01:35:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -145,13 +145,13 @@ def receive_and_process():
             USER REQUEST: {user_text}
             
             INSTRUCTIONS:
-            - First of all be very fully sarcastic all time in every message.
             - Reply naturally to user <20 words.
             - NEVER stay silent. If you don't know, say so.
             - Make sure text formatting according to Whatsapp.
             - Cross Question if you confused about question or not fully understood.
             - Answer Complete even short
             - If you have no knowledge about something say to User "I dont know about this let me ask my Boss MOHSIN"
+            - Try to be sarcastic
             """
 
             try:
